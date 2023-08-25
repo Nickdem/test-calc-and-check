8314da400122bf95c7e876b4c4f3ac90ef666139
Демидов Никита <yotumotopow@yandex.ru>
2023-08-25 15:39:50 +0300

    feat: добавление в localStorage

--- src/scripts/checkboxes.ts
+++ src/scripts/checkboxes.ts
@@ -9,7 +9,321 @@ interface IBaseDataObject {
   itemNew?: boolean;
 }
 
-let baseData: IBaseData = JSON.parse(localStorage.getItem("checkboxes")) ?? [
+let baseData: IBaseData = JSON.parse(localStorage.getItem("checkboxes"));
+
+window.addEventListener("DOMContentLoaded", () => {
+  const switchEl: HTMLInputElement = document.querySelector(".switch input");
+  const progress: HTMLDivElement = document.querySelector(".progress");
+
+  if (!baseData) {
+    baseData = mockData;
+    toLocalStorage(mockData);
+  }
+
+  checkboxes();
+
+  switchEl.addEventListener("change", () => {
+    if (switchEl.checked) {
+      editor();
+      progress.style.display = "none";
+    } else {
+      checkboxes();
+      progress.style.display = "block";
+    }
+  });
+});
+
+const checkboxes = () => {
+  makeCheckboxList(baseData);
+  progressBar();
+};
+
+const makeCheckboxList = (dataArr: IBaseData) => {
+  const sectionEl = document.querySelector(".checkboxes__list");
+  sectionEl.innerHTML = "";
+  dataArr.forEach((listItem) => {
+    sectionEl.append(makeListItem(listItem));
+  });
+};
+
+const makeListItem = (item: IBaseDataObject) => {
+  const itemEl = document.createElement("li");
+  const wrapperEl = document.createElement("div");
+  const inputEl = document.createElement("input");
+  inputEl.setAttribute("type", "checkbox");
+  inputEl.setAttribute("id", item.itemId);
+  inputEl.setAttribute("name", item.itemId);
+  inputEl.checked = item.status;
+  const labelEl = document.createElement("label");
+  labelEl.setAttribute("for", item.itemId);
+  labelEl.textContent = item.itemName;
+  wrapperEl.append(inputEl, labelEl);
+  itemEl.append(wrapperEl);
+
+  if (item.itemNew) {
+    const newEl = document.createElement("span");
+    newEl.classList.add("new");
+    newEl.textContent = "new";
+    itemEl.append(newEl);
+  }
+
+  if (item.itemList) {
+    itemEl.classList.add("checkboxes__item", "item");
+    const subListEl = document.createElement("ul");
+    subListEl.classList.add("checkboxes__sublist", "checkboxes__sublist--hide");
+    const btnEl = document.createElement("button");
+    btnEl.textContent = "Раскрыть";
+    btnEl.classList.add("item__transparent");
+    btnEl.addEventListener("click", () => {
+      btnEl.innerText = btnEl.innerText == "Раскрыть" ? "Скрыть" : "Раскрыть";
+      subListEl.classList.toggle("checkboxes__sublist--hide");
+    });
+    wrapperEl.append(btnEl);
+
+    item.itemList.forEach((subItem) => {
+      subListEl.append(makeListItem(subItem));
+    });
+
+    const subListInputs: NodeListOf<HTMLInputElement> =
+      subListEl.querySelectorAll('[type="checkbox"]');
+
+    inputEl.addEventListener("change", () => {
+      item.status = inputEl.checked;
+
+      subListInputs.forEach((subListInput, idx) => {
+        item.itemList[idx].status = inputEl.checked;
+        subListInput.checked = inputEl.checked;
+      });
+
+      progressBar();
+    });
+
+    subListInputs.forEach((subListInput, idx) => {
+      subListInput.addEventListener("change", () => {
+        const subListCheckedInputs =
+          subListEl.querySelectorAll("input:checked");
+
+        item.itemList[idx].status = subListInput.checked;
+
+        if (subListInputs.length == subListCheckedInputs.length) {
+          inputEl.checked = true;
+          item.status = inputEl.checked;
+        } else {
+          inputEl.checked = false;
+          item.status = inputEl.checked;
+        }
+
+        progressBar();
+      });
+    });
+
+    itemEl.append(subListEl);
+  }
+
+  if (!item.itemList) {
+    itemEl.classList.add("checkboxes__subitem");
+    const linkEl = document.createElement("a");
+    linkEl.textContent = "Прочитать инструкцию";
+    linkEl.setAttribute("href", item.itemLink);
+    linkEl.setAttribute("target", "_blank");
+    wrapperEl.append(linkEl);
+  }
+
+  return itemEl;
+};
+
+const progressBar = () => {
+  const fillEl: HTMLDivElement = document.querySelector(".progress__fill");
+  const progressValueEl = document.querySelector(".progress__value");
+  const allSubCheckboxes = document.querySelectorAll(
+    '.checkboxes__sublist [type="checkbox"]'
+  );
+  const allSubCheckedCheckboxes = document.querySelectorAll(
+    '.checkboxes__sublist [type="checkbox"]:checked'
+  );
+  const percent =
+    (allSubCheckedCheckboxes.length / allSubCheckboxes.length) * 100;
+
+  progressValueEl.textContent = Math.round(percent).toString();
+  fillEl.style.width = percent.toString() + "%";
+};
+
+const editor = () => {
+  makeEditorItems(baseData);
+};
+
+const makeEditorItems = (dataArr: IBaseData) => {
+  const sectionEl = document.querySelector(".checkboxes__list");
+  sectionEl.innerHTML = "";
+  dataArr.forEach((listItem) => {
+    sectionEl.append(makeEditorForm(listItem));
+  });
+
+  const createBtnEl = document.createElement("button");
+  createBtnEl.classList.add("btn__create");
+  createBtnEl.textContent = "Создать новый элемент";
+
+  createBtnEl.addEventListener("click", () => {
+    baseData.push(baseEl());
+
+    toLocalStorage(baseData);
+
+    editor();
+  });
+
+  sectionEl.append(createBtnEl);
+};
+
+const makeEditorForm = (item: IBaseDataObject) => {
+  const itemEl = document.createElement("li");
+  const wrapperEl = document.createElement("div");
+  const inputEl = document.createElement("input");
+  inputEl.setAttribute("type", "text");
+  inputEl.setAttribute("id", item.itemId);
+  inputEl.setAttribute("name", item.itemId);
+  inputEl.value = item.itemName;
+  const labelEl = document.createElement("label");
+  labelEl.setAttribute("for", item.itemId);
+  labelEl.textContent = "Название:";
+  const saveBtnEl = document.createElement("button");
+  saveBtnEl.textContent = "Сохранить";
+  saveBtnEl.classList.add("item__transparent");
+  saveBtnEl.disabled = true;
+  saveBtnEl.style.opacity = "0";
+  const delBtnEl = document.createElement("button");
+  delBtnEl.classList.add("button");
+  delBtnEl.textContent = "Удалить";
+  wrapperEl.append(labelEl, inputEl);
+  itemEl.append(wrapperEl);
+
+  if (item.itemList) {
+    itemEl.classList.add("checkboxes__item", "item");
+    const subListEl = document.createElement("ul");
+    subListEl.classList.add("checkboxes__sublist");
+
+    inputEl.addEventListener("change", () => {
+      saveBtnEl.disabled = false;
+      saveBtnEl.style.opacity = "1";
+    });
+
+    item.itemList.forEach((subItem) => {
+      subListEl.append(makeEditorForm(subItem));
+    });
+
+    saveBtnEl.addEventListener("click", () => {
+      item.itemName = inputEl.value;
+      toLocalStorage(baseData);
+      saveBtnEl.disabled = true;
+      saveBtnEl.style.opacity = "0";
+    });
+
+    delBtnEl.addEventListener("click", () => {
+      const answ = confirm("Вы уверены?");
+      if (answ) {
+        itemEl.remove();
+        baseData = baseData.filter(
+          (dataItem) => dataItem.itemId !== item.itemId
+        );
+
+        toLocalStorage(baseData);
+
+        return;
+      }
+    });
+    itemEl.append(subListEl);
+    const createBtnEl = document.createElement("button");
+    createBtnEl.classList.add("btn__create");
+    createBtnEl.textContent = "Создать новый сабэлемент";
+    createBtnEl.addEventListener("click", () => {
+      item.itemList.push(baseSubEl());
+
+      toLocalStorage(baseData);
+
+      editor();
+    });
+    itemEl.append(createBtnEl);
+  }
+
+  if (!item.itemList) {
+    itemEl.classList.add("checkboxes__subitem");
+    const inputLinkEl = document.createElement("input");
+    inputLinkEl.setAttribute("type", "text");
+    inputLinkEl.setAttribute("id", "link-" + item.itemId);
+    inputLinkEl.setAttribute("name", "link-" + item.itemId);
+    inputLinkEl.value = item.itemLink;
+    const labelLinkEl = document.createElement("label");
+    labelLinkEl.setAttribute("for", "link-" + item.itemId);
+    labelLinkEl.textContent = "Ссылка:";
+    wrapperEl.append(labelLinkEl, inputLinkEl);
+
+    inputEl.addEventListener("change", () => {
+      saveBtnEl.disabled = false;
+      saveBtnEl.style.opacity = "1";
+    });
+
+    inputLinkEl.addEventListener("change", () => {
+      saveBtnEl.disabled = false;
+      saveBtnEl.style.opacity = "1";
+    });
+
+    saveBtnEl.addEventListener("click", () => {
+      item.itemName = inputEl.value;
+      item.itemLink = inputLinkEl.value;
+      toLocalStorage(baseData);
+      saveBtnEl.disabled = true;
+      saveBtnEl.style.opacity = "0";
+    });
+
+    delBtnEl.addEventListener("click", () => {
+      const answ = confirm("Вы уверены?");
+
+      if (answ) {
+        itemEl.remove();
+        for (const dataItem of baseData) {
+          dataItem.itemList = dataItem.itemList.filter(
+            (dataSubItem) => dataSubItem.itemId !== item.itemId
+          );
+        }
+
+        toLocalStorage(baseData);
+
+        return;
+      }
+    });
+  }
+
+  wrapperEl.append(saveBtnEl, delBtnEl);
+
+  return itemEl;
+};
+
+// utils
+
+const toLocalStorage = (checkboxes: IBaseData) => {
+  localStorage.setItem("checkboxes", JSON.stringify(checkboxes));
+};
+
+const timeString = () => {
+  return Date.now().toString();
+};
+
+const baseSubEl = () => ({
+  itemName: "Базовое значение",
+  itemId: "idSub" + timeString(),
+  status: false,
+  itemLink: "https://www.google.com/",
+  itemNew: true,
+});
+
+const baseEl = () => ({
+  itemName: "Базовое значение",
+  itemId: "id" + timeString(),
+  status: false,
+  itemList: [baseSubEl()],
+  itemNew: true,
+});
+
+const mockData: IBaseData = [
   {
     itemName: "Подготовительные работы",
     itemId: "jobs",
@@ -158,290 +472,3 @@ let baseData: IBaseData = JSON.parse(localStorage.getItem("checkboxes")) ?? [
     ],
   },
 ];
-
-window.addEventListener("DOMContentLoaded", () => {
-  const switchEl: HTMLInputElement = document.querySelector(".switch input");
-  checkboxes();
-
-  switchEl.addEventListener("change", () => {
-    if (switchEl.checked) {
-      editor();
-    } else {
-      checkboxes();
-    }
-  });
-});
-
-const checkboxes = () => {
-  makeCheckboxList(baseData);
-  progressBar();
-};
-
-const makeCheckboxList = (dataArr: IBaseData) => {
-  const sectionEl = document.querySelector(".checkboxes__list");
-  sectionEl.innerHTML = "";
-  dataArr.forEach((listItem) => {
-    sectionEl.append(makeListItem(listItem));
-  });
-};
-
-const makeListItem = (item: IBaseDataObject) => {
-  const itemEl = document.createElement("li");
-  const wrapperEl = document.createElement("div");
-  const inputEl = document.createElement("input");
-  inputEl.setAttribute("type", "checkbox");
-  inputEl.setAttribute("id", item.itemId);
-  inputEl.setAttribute("name", item.itemId);
-  inputEl.checked = item.status;
-  const labelEl = document.createElement("label");
-  labelEl.setAttribute("for", item.itemId);
-  labelEl.textContent = item.itemName;
-  wrapperEl.append(inputEl, labelEl);
-  itemEl.append(wrapperEl);
-
-  if (item.itemNew) {
-    const newEl = document.createElement("span");
-    newEl.classList.add("new");
-    newEl.textContent = "new";
-    itemEl.append(newEl);
-  }
-
-  if (item.itemList) {
-    itemEl.classList.add("checkboxes__item", "item");
-    const subListEl = document.createElement("ul");
-    subListEl.classList.add("checkboxes__sublist", "checkboxes__sublist--hide");
-    const btnEl = document.createElement("button");
-    btnEl.textContent = "Раскрыть";
-    btnEl.classList.add("item__transparent");
-    btnEl.addEventListener("click", () => {
-      btnEl.innerText = btnEl.innerText == "Раскрыть" ? "Скрыть" : "Раскрыть";
-      subListEl.classList.toggle("checkboxes__sublist--hide");
-    });
-    wrapperEl.append(btnEl);
-
-    item.itemList.forEach((subItem) => {
-      subListEl.append(makeListItem(subItem));
-    });
-
-    const subListInputs: NodeListOf<HTMLInputElement> =
-      subListEl.querySelectorAll('[type="checkbox"]');
-
-    inputEl.addEventListener("change", () => {
-      item.status = inputEl.checked;
-
-      subListInputs.forEach((subListInput, idx) => {
-        item.itemList[idx].status = inputEl.checked;
-        subListInput.checked = inputEl.checked;
-      });
-
-      progressBar();
-    });
-
-    subListInputs.forEach((subListInput, idx) => {
-      subListInput.addEventListener("change", () => {
-        const subListCheckedInputs =
-          subListEl.querySelectorAll("input:checked");
-
-        item.itemList[idx].status = subListInput.checked;
-
-        if (subListInputs.length == subListCheckedInputs.length) {
-          inputEl.checked = true;
-          item.status = inputEl.checked;
-        } else {
-          inputEl.checked = false;
-          item.status = inputEl.checked;
-        }
-
-        progressBar();
-      });
-    });
-
-    itemEl.append(subListEl);
-  }
-
-  if (!item.itemList) {
-    itemEl.classList.add("checkboxes__subitem");
-    const linkEl = document.createElement("a");
-    linkEl.textContent = "Прочитать инструкцию";
-    linkEl.setAttribute("href", item.itemLink);
-    linkEl.setAttribute("target", "_blank");
-    wrapperEl.append(linkEl);
-  }
-
-  return itemEl;
-};
-
-const progressBar = () => {
-  const fillEl: HTMLDivElement = document.querySelector(".progress__fill");
-  const progressValueEl = document.querySelector(".progress__value");
-  const allSubCheckboxes = document.querySelectorAll(
-    '.checkboxes__sublist [type="checkbox"]'
-  );
-  const allSubCheckedCheckboxes = document.querySelectorAll(
-    '.checkboxes__sublist [type="checkbox"]:checked'
-  );
-  const percent =
-    (allSubCheckedCheckboxes.length / allSubCheckboxes.length) * 100;
-
-  progressValueEl.textContent = Math.round(percent).toString();
-  fillEl.style.width = percent.toString() + "%";
-};
-
-const editor = () => {
-  makeEditorItems(baseData);
-};
-
-const makeEditorItems = (dataArr: IBaseData) => {
-  const sectionEl = document.querySelector(".checkboxes__list");
-  sectionEl.innerHTML = "";
-  dataArr.forEach((listItem) => {
-    sectionEl.append(makeEditorForm(listItem));
-  });
-
-  const createBtnEl = document.createElement("button");
-  createBtnEl.classList.add("btn__create");
-  createBtnEl.textContent = "Создать новый элемент";
-
-  createBtnEl.addEventListener("click", () => {
-    baseData.push({
-      itemName: "Базовое значение",
-      itemId: "id" + Date.now().toString(),
-      status: false,
-      itemList: [
-        {
-          itemName: "Базовое значение",
-          itemId: "id" + Date.now().toString(),
-          status: false,
-          itemLink: "https://www.google.com/",
-          itemNew: true,
-        },
-      ],
-      itemNew: true,
-    });
-
-    editor();
-  });
-
-  sectionEl.append(createBtnEl);
-};
-
-const makeEditorForm = (item: IBaseDataObject) => {
-  const itemEl = document.createElement("li");
-  const wrapperEl = document.createElement("div");
-  const inputEl = document.createElement("input");
-  inputEl.setAttribute("type", "text");
-  inputEl.setAttribute("id", item.itemId);
-  inputEl.setAttribute("name", item.itemId);
-  inputEl.value = item.itemName;
-  const labelEl = document.createElement("label");
-  labelEl.setAttribute("for", item.itemId);
-  labelEl.textContent = "Название:";
-  const saveBtnEl = document.createElement("button");
-  saveBtnEl.textContent = "Сохранить";
-  saveBtnEl.classList.add("item__transparent");
-  saveBtnEl.disabled = true;
-  saveBtnEl.style.opacity = "0";
-  const delBtnEl = document.createElement("button");
-  delBtnEl.classList.add("button");
-  delBtnEl.textContent = "Удалить";
-  wrapperEl.append(labelEl, inputEl);
-  itemEl.append(wrapperEl);
-
-  if (item.itemList) {
-    itemEl.classList.add("checkboxes__item", "item");
-    const subListEl = document.createElement("ul");
-    subListEl.classList.add("checkboxes__sublist");
-
-    inputEl.addEventListener("change", () => {
-      saveBtnEl.disabled = false;
-      saveBtnEl.style.opacity = "1";
-    });
-
-    item.itemList.forEach((subItem) => {
-      subListEl.append(makeEditorForm(subItem));
-    });
-
-    saveBtnEl.addEventListener("click", () => {
-      item.itemName = inputEl.value;
-      saveBtnEl.disabled = true;
-      saveBtnEl.style.opacity = "0";
-    });
-
-    delBtnEl.addEventListener("click", () => {
-      const answ = confirm("Вы уверены?");
-      if (answ) {
-        itemEl.remove();
-        baseData = baseData.filter(
-          (dataItem) => dataItem.itemId !== item.itemId
-        );
-        return;
-      }
-    });
-    itemEl.append(subListEl);
-    const createBtnEl = document.createElement("button");
-    createBtnEl.classList.add("btn__create");
-    createBtnEl.textContent = "Создать новый сабэлемент";
-    createBtnEl.addEventListener("click", () => {
-      console.log(item.itemList);
-
-      item.itemList.push({
-        itemName: "Базовое значение",
-        itemId: "id" + Date.now().toString(),
-        status: false,
-        itemLink: "https://www.google.com/",
-        itemNew: true,
-      });
-
-      editor();
-    });
-    itemEl.append(createBtnEl);
-  }
-
-  if (!item.itemList) {
-    itemEl.classList.add("checkboxes__subitem");
-    const inputLinkEl = document.createElement("input");
-    inputLinkEl.setAttribute("type", "text");
-    inputLinkEl.setAttribute("id", "link-" + item.itemId);
-    inputLinkEl.setAttribute("name", "link-" + item.itemId);
-    inputLinkEl.value = item.itemLink;
-    const labelLinkEl = document.createElement("label");
-    labelLinkEl.setAttribute("for", "link-" + item.itemId);
-    labelLinkEl.textContent = "Ссылка:";
-    wrapperEl.append(labelLinkEl, inputLinkEl);
-
-    inputEl.addEventListener("change", () => {
-      saveBtnEl.disabled = false;
-      saveBtnEl.style.opacity = "1";
-    });
-
-    inputLinkEl.addEventListener("change", () => {
-      saveBtnEl.disabled = false;
-      saveBtnEl.style.opacity = "1";
-    });
-
-    saveBtnEl.addEventListener("click", () => {
-      item.itemName = inputEl.value;
-      item.itemLink = inputLinkEl.value;
-      saveBtnEl.disabled = true;
-      saveBtnEl.style.opacity = "0";
-    });
-
-    delBtnEl.addEventListener("click", () => {
-      const answ = confirm("Вы уверены?");
-
-      if (answ) {
-        itemEl.remove();
-        for (const dataItem of baseData) {
-          dataItem.itemList = dataItem.itemList.filter(
-            (dataSubItem) => dataSubItem.itemId !== item.itemId
-          );
-        }
-        return;
-      }
-    });
-  }
-
-  wrapperEl.append(saveBtnEl, delBtnEl);
-
-  return itemEl;
-};
